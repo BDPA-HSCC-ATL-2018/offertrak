eb62764675e6441cd89e5caeb6c05f0cd360adfa
wilfred66 <wilfredpowelljr@gmail.com>
2019-07-25 17:41:16 -0400

    mhmm

--- web-assets/tpl/app_header.php
+++ web-assets/tpl/app_header.php
@@ -1,9 +1,3 @@
-<?php session_start();
-error_reporting(E_ALL);
-ini_set("log_errors" , "1");
-ini_set("error_log" , "error_log");
-ini_set("display_errors" , "1");
-?>
 <!doctype html>
 <html lang="en">
   <head>
