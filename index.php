3ddad06271f1df08c48dab6cb8e2249337293aa9
wilfred66 <wilfredpowelljr@gmail.com>
2019-07-24 21:45:51 -0400

    index controller...I think

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php
   session_start()
-  include_once $_SERVER['DOCUMENT_ROOT'] . "/offertrack/db.php"
+  include_once $_SERVER['DOCUMENT_ROOT'] . "/offertrack/db.php";
 
-  }
+  $action = isset($_REQUEST['action']) ? $_REQUEST['action'] : null;
+
+  $options = array (
+    'signup' => 'signup',
+    'login' => 'login',
+    'jobapplicantlist' => 'jobapplicantlist',
+    'jobapplicantreport' => 'jobapplicantreport',
+    'edituser' => 'edituser',
+    'logout' => 'logout',
+    'admin' => 'admin'
+    //Dont know how to break down the whole admin thing
+  );
 
 
  ?>
