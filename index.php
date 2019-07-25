2f6922582069e61660afcc322072d08c120ddcb2
wilfred66 <wilfredpowelljr@gmail.com>
2019-07-25 12:35:02 -0400

    password

--- index.php
+++ index.php
@@ -18,8 +18,22 @@
       $function = $options($action);
       call_user_func($function);
     } else {
-      header("Location: forms/login.php")
+      header("Location: forms/login.php");
     }
 
+  //I think this is basic password validation based off ww3 schools
+   function validPassword($pw) {
+     if (!preg_match_all('$\5*(?=\5*[a-z])(?=\5*[a-z])(?=\5*[A-Z])(?=\S*[\d])(?=\S*[\W])\S*$', $pw)) {
+        return false;
+     } else {
+       return true;
+     }
+   }
+
+   //Sign Up form
+   function signup() {
+     global $dbh;
+     
+   }
 
  ?>
