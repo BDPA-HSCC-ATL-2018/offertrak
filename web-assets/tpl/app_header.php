20fadd6f92ff96ade7dad95c7f111f67d425263c
wilfred66 <wilfredpowelljr@gmail.com>
2019-07-25 18:06:54 -0400

    ...

--- web-assets/tpl/app_header.php
+++ web-assets/tpl/app_header.php
@@ -7,8 +7,6 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="/web-assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/web-assets/css/bdpa.css">
     <title><?php echo $page_title; ?></title>
   </head>
   <body>
-    <?php include_once $_SERVER['DOCUMENT_ROOT'] . "/lib/db.php"; ?>
